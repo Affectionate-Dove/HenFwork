afdfa500bdb679ce400b8b4a9efbde74438062d5
Hōtarō Oreki <TheZjarek@gmail.com>
2022-02-05 16:38:45 +0100

    Make toolbars be seperate classes

---
+++ HenFwork.MapEditing/Screens/Editor/ActionsToolbar.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenBstractions.Graphics;
+using HenFwork.Graphics2d;
+using System.Collections.Generic;
+
+namespace HenFwork.MapEditing.Screens.Editor
+{
+    public class ActionsToolbar : Container
+    {
+        public ActionsToolbar()
+        {
+            RelativeSizeAxes = Axes.X;
+
+            // background
+            AddChild(new Rectangle
+            {
+                RelativeSizeAxes = Axes.Both,
+                Color = new(128, 128, 128)
+            });
+
+            const float spacing = 3;
+            var flowContainer = new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding
+                {
+                    Horizontal = spacing,
+                    Vertical = spacing,
+                },
+                Spacing = spacing,
+                Direction = Direction.Horizontal
+            };
+            AddChild(flowContainer);
+
+            foreach (var sampleButton in CreatePlaceholderActionButtons())
+                flowContainer.AddChild(sampleButton);
+        }
+
+        private static IEnumerable<Rectangle> CreatePlaceholderActionButtons()
+        {
+            var colors = new ColorInfo[]
+            {
+                ColorInfo.ORANGE,
+                ColorInfo.VIOLET,
+                new(0, 122, 32),
+                new(0, 51, 122),
+                new(122, 0, 76),
+                new(100, 122, 0),
+                ColorInfo.MAROON,
+                ColorInfo.DARKPURPLE
+            };
+
+            foreach (var color in colors)
+            {
+                yield return new Rectangle
+                {
+                    FillMode = FillMode.Fit,
+                    RelativeSizeAxes = Axes.Both,
+                    BorderThickness = 3,
+                    BorderColor = new(255, 255, 255, 200), //new(200, 200, 200),
+                    Color = color
+                };
+            }
+        }
+    }
+}
---
+++ HenFwork.MapEditing/Screens/Editor/ToolsToolbar.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenFwork.Graphics2d;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HenFwork.MapEditing.Screens.Editor
+{
+    public class ToolsToolbar : Container
+    {
+        private const string cursor_img_path = @"Resources\Images\Tools\cursor.png";
+        private const string add_img_path = @"Resources\Images\Tools\add.png";
+        private const string rotate_img_path = @"Resources\Images\Tools\rotate.png";
+        private const string resize_img_path = @"Resources\Images\Tools\resize.png";
+        private static readonly string[] toolImgs = new[] { cursor_img_path, add_img_path, rotate_img_path, resize_img_path };
+
+        public ToolsToolbar()
+        {
+            foreach (var toolImg in toolImgs.Where(toolImg => !Game.TextureStore.IsLoaded(toolImg)))
+                Game.TextureStore.Load(toolImg).GenerateMipmaps();
+
+            RelativeSizeAxes = Axes.Y;
+
+            // background
+            AddChild(new Rectangle
+            {
+                RelativeSizeAxes = Axes.Both,
+                Color = new(64, 64, 64)
+            });
+
+            const float spacing = 4;
+            var flowContainer = new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding
+                {
+                    Horizontal = spacing,
+                    Vertical = spacing,
+                },
+                Spacing = spacing,
+                Direction = Direction.Vertical
+            };
+            AddChild(flowContainer);
+
+            foreach (var sampleButton in CreatePlaceholderTools())
+                flowContainer.AddChild(sampleButton);
+        }
+
+        private static IEnumerable<Drawable> CreatePlaceholderTools()
+        {
+            static Drawable createPlaceholderTool(string imgPath)
+            {
+                var container = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    FillMode = FillMode.Fit,
+                };
+
+                container.AddChild(new Rectangle
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Color = new(150, 150, 150, 255),
+                    BorderColor = new(255, 255, 255, 90),
+                    BorderThickness = 3
+                });
+
+                container.AddChild(new Sprite
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    FillMode = FillMode.Fit,
+                    AutoFillModeProportions = true,
+                    Size = new(0.6f),
+                    Anchor = new(0.5f),
+                    Origin = new(0.5f),
+                    Texture = Game.TextureStore.Get(imgPath)
+                });
+
+                return container;
+            }
+
+            foreach (var toolImg in toolImgs)
+                yield return createPlaceholderTool(toolImg);
+        }
+    }
+}
