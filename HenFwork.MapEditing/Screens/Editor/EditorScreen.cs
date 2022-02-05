8725f512a1ed57f0ab2e195f1c26e5c1a69373e8
Hōtarō Oreki <TheZjarek@gmail.com>
2022-02-05 02:59:26 +0100

    HEN-151 - Very basic layout for Map Editor Screen

---
+++ HenFwork.MapEditing/Screens/Editor/EditorScreen.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenBstractions.Graphics;
+using HenFwork.Graphics2d;
+using HenFwork.Screens;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HenFwork.MapEditing.Screens.Editor
+{
+    public class EditorScreen : Screen
+    {
+        private const float ACTIONS_TOOLBAR_SIZE = 40;
+        private const float TOOLS_TOOLBAR_SIZE = 50;
+
+        public EditorScreen() => CreateLayout();
+
+        private static Container CreateToolsToolbar()
+        {
+            var container = new Container
+            {
+                RelativeSizeAxes = Axes.Y,
+                Size = new(TOOLS_TOOLBAR_SIZE, 1),
+                Padding = new() { Top = ACTIONS_TOOLBAR_SIZE }
+            };
+
+            // background
+            container.AddChild(new Rectangle
+            {
+                RelativeSizeAxes = Axes.Both,
+                Color = new(64, 64, 64)
+            });
+
+            const float spacing = 6;
+            var flowContainer = new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding
+                {
+                    Horizontal = spacing,
+                    Vertical = spacing,
+                },
+                Spacing = spacing,
+                Direction = Direction.Vertical
+            };
+            container.AddChild(flowContainer);
+
+            foreach (var sampleButton in CreatePlaceholderActionButtons().Reverse())
+                flowContainer.AddChild(sampleButton);
+
+            return container;
+        }
+
+        private static Container CreateActionsToolbar()
+        {
+            var container = new Container
+            {
+                RelativeSizeAxes = Axes.X,
+                Size = new(1, ACTIONS_TOOLBAR_SIZE)
+            };
+
+            // background
+            container.AddChild(new Rectangle
+            {
+                RelativeSizeAxes = Axes.Both,
+                Color = new(128, 128, 128)
+            });
+
+            const float spacing = 2;
+            var flowContainer = new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding
+                {
+                    Horizontal = spacing,
+                    Vertical = spacing,
+                    Left = TOOLS_TOOLBAR_SIZE + spacing
+                },
+                Spacing = spacing,
+                Direction = Direction.Horizontal
+            };
+            container.AddChild(flowContainer);
+
+            foreach (var sampleButton in CreatePlaceholderActionButtons())
+                flowContainer.AddChild(sampleButton);
+
+            return container;
+        }
+
+        private static IEnumerable<Rectangle> CreatePlaceholderActionButtons()
+        {
+            var colors = new ColorInfo[]
+            {
+                ColorInfo.ORANGE,
+                ColorInfo.VIOLET,
+                new(0, 122, 32),
+                new(0, 51, 122),
+                new(122, 0, 76),
+                new(100, 122, 0),
+                ColorInfo.MAROON,
+                ColorInfo.DARKPURPLE
+            };
+
+            foreach (var color in colors)
+            {
+                yield return new Rectangle
+                {
+                    FillMode = FillMode.Fit,
+                    RelativeSizeAxes = Axes.Both,
+                    BorderThickness = 3,
+                    BorderColor = new(0,0,0, 100), //new(200, 200, 200),
+                    Color = color
+                };
+            }
+        }
+
+        private void CreateLayout()
+        {
+            AddChild(CreateToolsToolbar());
+            AddChild(CreateActionsToolbar());
+        }
+    }
+}
