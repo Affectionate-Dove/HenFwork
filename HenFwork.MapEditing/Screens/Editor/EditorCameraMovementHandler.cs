8933db5094ef3806c2049c5098ea308fe47a8760
Maciej Klimiuk <maciek120200@gmail.com>
2022-02-11 17:38:35 +0100

    Add EditorCameraMovementHandler class

---
+++ HenFwork.MapEditing/Screens/Editor/EditorCameraMovementHandler.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenBstractions.Graphics;
+using HenFwork.Graphics2d;
+using HenFwork.Input.UI;
+using HenFwork.MapEditing.Input;
+using System;
+using System.Numerics;
+
+namespace HenFwork.MapEditing.Screens.Editor
+{
+    public class EditorCameraMovementHandler : Container, IInterfaceComponent<EditorControls>
+    {
+        private const float camera_speed = 1.2f;
+        private readonly Graphics2d.Rectangle focusBorder;
+        private Vector3 cameraVelocity;
+        private WorldEditViewer worldEditViewer;
+
+        public event Action<IInterfaceComponent<EditorControls>> FocusRequested
+        { add { } remove { } }
+
+        public bool AcceptsFocus => true;
+
+        public EditorCameraMovementHandler(WorldEditViewer worldEditViewer)
+        {
+            this.worldEditViewer = worldEditViewer;
+            AddChild(focusBorder = new Graphics2d.Rectangle()
+            {
+                BorderThickness = 3,
+                RelativeSizeAxes = Axes.Both,
+                Color = ColorInfo.BLANK
+            });
+        }
+
+        public bool OnActionPressed(EditorControls action)
+        {
+            var cameraVelocityDelta = ActionCameraVelocityChange(action);
+            if (cameraVelocityDelta != Vector3.Zero)
+            {
+                cameraVelocity += cameraVelocityDelta;
+                return true;
+            }
+            return false;
+        }
+
+        public void OnActionReleased(EditorControls action) => cameraVelocity -= ActionCameraVelocityChange(action);
+
+        public void OnFocus() => focusBorder.BorderColor = ColorInfo.ORANGE;
+
+        public void OnFocusLost() => focusBorder.BorderColor = ColorInfo.BLANK;
+
+        protected override void OnUpdate(float elapsed)
+        {
+            base.OnUpdate(elapsed);
+            worldEditViewer.ObservedPoint += cameraVelocity * elapsed;
+        }
+
+        private static Vector3 ActionCameraVelocityChange(EditorControls action)
+        {
+            return action switch
+            {
+                EditorControls.MoveForward => new Vector3(0, 0, camera_speed),
+                EditorControls.MoveBackward => new Vector3(0, 0, -camera_speed),
+                EditorControls.MoveLeft => new Vector3(-camera_speed, 0, 0),
+                EditorControls.MoveRight => new Vector3(camera_speed, 0, 0),
+                EditorControls.MoveUp => new Vector3(0, camera_speed, 0),
+                EditorControls.MoveDown => new Vector3(0, -camera_speed, 0),
+                _ => Vector3.Zero
+            };
+        }
+    }
+}
--- HenFwork.MapEditing/Screens/Editor/EditorScreen.cs
+++ HenFwork.MapEditing/Screens/Editor/EditorScreen.cs
@@ -3,11 +3,7 @@
 // See the LICENSE file in the repository root for full license text.
 
 using HenFwork.Graphics2d;
-using HenFwork.Input.UI;
-using HenFwork.MapEditing.Input;
 using HenFwork.Screens;
-using System;
-using System.Numerics;
 
 namespace HenFwork.MapEditing.Screens.Editor
 {
@@ -16,65 +12,26 @@ namespace HenFwork.MapEditing.Screens.Editor
     ///     to select various tools and actions, and use
     ///     them on the map.
     /// </summary>
-    public class EditorScreen : Screen, IInterfaceComponent<EditorControls>
+    public class EditorScreen : Screen
     {
         private const float ACTIONS_TOOLBAR_SIZE = 40;
         private const float TOOLS_TOOLBAR_SIZE = 42;
         private const float toolbars_margins = 100;
-        private const float camera_speed = 1.2f;
         private WorldEditViewer worldEditViewer;
-        private Vector3 cameraVelocity;
 
-        public event Action<IInterfaceComponent<EditorControls>> FocusRequested
-        { add { } remove { } }
-
-        public bool AcceptsFocus => true;
-
-        public EditorScreen() => CreateLayout();
-
-        public bool OnActionPressed(EditorControls action)
+        public EditorScreen()
         {
-            var cameraVelocityDelta = ActionCameraVelocityChange(action);
-            if (cameraVelocityDelta != Vector3.Zero)
+            worldEditViewer = new WorldEditViewer();
+            AddChild(worldEditViewer);
+            AddChild(new EditorCameraMovementHandler(worldEditViewer)
             {
-                cameraVelocity += cameraVelocityDelta;
-                return true;
-            }
-            return false;
-        }
-
-        public void OnActionReleased(EditorControls action) => cameraVelocity -= ActionCameraVelocityChange(action);
-
-        public void OnFocus()
-        { }
-
-        public void OnFocusLost()
-        { }
-
-        protected override void OnUpdate(float elapsed)
-        {
-            base.OnUpdate(elapsed);
-            worldEditViewer.ObservedPoint += cameraVelocity * elapsed;
-        }
-
-        private static Vector3 ActionCameraVelocityChange(EditorControls action)
-        {
-            return action switch
-            {
-                EditorControls.MoveForward => new Vector3(0, 0, camera_speed),
-                EditorControls.MoveBackward => new Vector3(0, 0, -camera_speed),
-                EditorControls.MoveLeft => new Vector3(-camera_speed, 0, 0),
-                EditorControls.MoveRight => new Vector3(camera_speed, 0, 0),
-                EditorControls.MoveUp => new Vector3(0, camera_speed, 0),
-                EditorControls.MoveDown => new Vector3(0, -camera_speed, 0),
-                _ => Vector3.Zero
-            };
+                RelativeSizeAxes = Axes.Both
+            });
+            CreateLayout();
         }
 
         private void CreateLayout()
         {
-            AddChild(worldEditViewer = new WorldEditViewer());
-
             AddChild(new ToolsToolbar
             {
                 RelativeSizeAxes = Axes.Y,
